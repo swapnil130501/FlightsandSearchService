6f595f2ad70070a9bfb369b42c58bf5eb0ef749a
swapnil <swapnil130501@gmail.com>
2023-05-23 14:29:38 +0530

    setup basic body parser

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ const setupAndStartServer = async() => {
     
     //create the express object
     const app = express();
+    
     app.use(bodypasrser.json());
     app.use(bodypasrser.urlencoded({extended: true}));
-    
+
     app.listen(PORT,() => {
         console.log(`server started at ${PORT}`);
     });
