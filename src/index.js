dfc7ed3a3b98dd7e0805a2e80eb9c36c3f173304
swapnil <swapnil130501@gmail.com>
2023-05-24 11:04:49 +0530

    added unique constraint and started CityRepository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodypasrser = require('body-parser');
 
+const CityRepository = require('./repository/city-repository');
 const { PORT } = require('./config/serverConfig')
 
 const setupAndStartServer = async() => {
@@ -11,8 +12,10 @@ const setupAndStartServer = async() => {
     app.use(bodypasrser.json());
     app.use(bodypasrser.urlencoded({extended: true}));
 
-    app.listen(PORT,() => {
+    app.listen(PORT, async() => {
         console.log(`server started at ${PORT}`);
+        const repo = new CityRepository();
+        repo.createCity({name: "New Delhi"});
     });
 }
 setupAndStartServer();
