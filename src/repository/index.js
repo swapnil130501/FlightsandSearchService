5d331c2525929aef5a4b78f0d4ddb8910578a6c6
swapnil <swapnil130501@gmail.com>
2023-05-27 14:49:23 +0530

    added common CRUD classes for basic CRUD operations

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,5 +1,7 @@
 module.exports = {
     CityRepository: require('./city-repository'),
-    FlightRespository: require('./flight-repository'),
-    AirplaneRespository: require('./airplane-repository')
+    FlightRepository: require('./flight-repository'),
+    AirplaneRepository: require('./airplane-repository'),
+    AirportRepository: require('./airport-repository'),
+    CrudRepository: require('./crud-repository')
 }
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,11 +1,11 @@
-const {FlightRespository, AirplaneRespository} = require('../repository/index');
+const {FlightRepository, AirplaneRepository} = require('../repository/index');
 const { compareTime } = require('../utils/helper');
 
 class FlightService {
 
     constructor() {
-        this.airplaneRespository = new AirplaneRespository();
-        this.flightrespository = new FlightRespository();
+        this.airplaneRepository = new AirplaneRepository();
+        this.flightRepository = new FlightRepository();
     }
 
     async createFlight(data) {
@@ -13,8 +13,8 @@ class FlightService {
             if(!compareTime(data.arrivalTime, data.departureTime)) {
                 throw {error: 'Arrival time cannot be less than departure time'};
             }
-            const airplane = await this.airplaneRespository.getAirplane(data.airplaneId);
-            const flight = await this.flightrespository.createFlight({
+            const airplane = await this.airplaneRepository.getAirplane(data.airplaneId);
+            const flight = await this.flightRepository.createFlight({
                 ...data, totalSeats:airplane.capacity 
             });
             return flight;
@@ -30,7 +30,7 @@ class FlightService {
 
     async getAllFlightData(data){
         try {
-            const flights = await this.flightrespository.getAllFlights(data);
+            const flights = await this.flightRepository.getAllFlights(data);
             return flights;
         } catch (error) {
             console.log("Something went wrong at service layer");
@@ -44,7 +44,7 @@ module.exports = FlightService;
 /**
  * {
  *   flightNumber,
- *  airplaneId ,
+ *   airplaneId,
  *   departureAirportId,
  *   arrivalAirportId,
  *   arrivalTime,
